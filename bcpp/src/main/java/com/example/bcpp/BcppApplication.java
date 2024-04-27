669cc8beefa66a1d211e20eb04020a168c7ca23b
stjepanoviccc <stjepanoviccc2021@gmail.com>
2024-04-27 03:18:22 +0200

    SpringBoot App Created

---
+++ bcpp/src/main/java/com/example/bcpp/BcppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bcpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcppApplication.class, args);
+	}
+
+}
---
+++ bcpp/src/test/java/com/example/bcpp/BcppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bcpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
