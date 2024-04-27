e4223a2ec38b891c8199dc1b84881aac28cb36ab
stjepanoviccc <stjepanoviccc2021@gmail.com>
2024-04-27 13:31:04 +0200

    basic security and exception setup impl

---
+++ bcpp/src/main/java/com/example/bcpp/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.bcpp.service.impl;
+
+import com.example.bcpp.exception.NotFoundException;
+import com.example.bcpp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new NotFoundException(String.format("User with email %s not found.", email)));
+    }
+}
